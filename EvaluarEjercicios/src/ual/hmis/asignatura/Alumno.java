184080e4d500ef7eda822579358dacbdef5e6d17
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-04-10 10:57:01 +0200

    ejemplo base evaluar alumnos

---
+++ EvaluarEjercicios/src/ual/hmis/asignatura/Alumno.java
@@ -0,0 +1,32 @@
+package ual.hmis.asignatura;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Alumno {
+
+	private String nombre;
+	private ArrayList<Actividad> actividadesAsignadas;
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public ArrayList<Actividad> getActividadesAsignadas() {
+		return actividadesAsignadas;
+	}
+
+	public void setActividadesAsignadas(ArrayList<Actividad> actividadesAsignadas) {
+		this.actividadesAsignadas = actividadesAsignadas;
+	}
+
+	public double calcularNotaActividad(String nombreActividad) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ EvaluarEjercicios/src/ual/hmis/asignatura/Ejercicio.java
@@ -0,0 +1,23 @@
+package ual.hmis.asignatura;
+
+public class Ejercicio {
+
+	private String nombre;
+	private double puntuacion;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(double puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+}
